ba7e44ea2d2af1893df1cf061c9b7d2a73b8ab89
ProjectEight <s.frost@frostnet.co.uk>
2019-03-12 19:27:18 +0000

    Stripped out a lot of features to make it load in a reasonable time; Added zebra-striping of rows

--- res/XdebugParser.php
+++ res/XdebugParser.php
@@ -28,11 +28,6 @@ class XdebugParser
         if (!$this->handle) {
             throw new Exception("Can't open '$fileName'");
         }
-        $header1 = fgets($this->handle);
-        $header2 = fgets($this->handle);
-        if (!preg_match('@Version: [23].*@', $header1) || !preg_match('@File format: [2-4]@', $header2)) {
-            throw new Exception("This file is not an Xdebug trace file made with format option '1' and version 2 to 4.");
-        }
     }
 
     /**
@@ -52,42 +47,27 @@ class XdebugParser
     public function parseLine($line)
     {
         $parts = explode("\t", $line);
-        if (count($parts) < 5) {
-            return;
-        }
 
-        $funcNr = $parts[1];
-        switch ($parts[2]) {
+        switch (@$parts[2]) {
             case '0': // Function enter
-                $this->functions[$funcNr]                 = [];
-                $this->functions[$funcNr]['depth']        = (int)$parts[0];
-                $this->functions[$funcNr]['time.enter']   = $parts[3];
-                $this->functions[$funcNr]['memory.enter'] = $parts[4];
-                $this->functions[$funcNr]['name']         = $parts[5];
-                $this->functions[$funcNr]['internal']     = !(bool)$parts[6]; // Refactor so we can exclude this
-                $this->functions[$funcNr]['file']         = $parts[8];
-                $this->functions[$funcNr]['line']         = $parts[9];
+                $this->functions[$parts[1]]['depth']        = (int)$parts[0];
+                $this->functions[$parts[1]]['time.enter']   = $parts[3];
+                $this->functions[$parts[1]]['name']         = $parts[5];
+                $this->functions[$parts[1]]['file']         = $parts[8];
+                $this->functions[$parts[1]]['line']         = $parts[9];
                 if ($parts[7]) {
-                    $this->functions[$funcNr]['params'] = [$parts[7]];
-                } else {
-                    $this->functions[$funcNr]['params'] = array_slice($parts, 11);
+                    $this->functions[$parts[1]]['params'] = [$parts[7]];
                 }
 
                 // these are set later // Necessary to do this here?
-                $this->functions[$funcNr]['time.exit']   = '';
-                $this->functions[$funcNr]['memory.exit'] = '';
-                $this->functions[$funcNr]['time.diff']   = '';
-                $this->functions[$funcNr]['memory.diff'] = '';
-                $this->functions[$funcNr]['return']      = '';
+                $this->functions[$parts[1]]['return']      = '';
                 break;
             case '1': // Function exit
-                $this->functions[$funcNr]['time.exit']   = $parts[3];
-                $this->functions[$funcNr]['memory.exit'] = $parts[4];
-                $this->functions[$funcNr]['time.diff']   = $this->functions[$funcNr]['time.exit'] - $this->functions[$funcNr]['time.enter'];
-                $this->functions[$funcNr]['memory.diff'] = (int)$this->functions[$funcNr]['memory.exit'] - (int)$this->functions[$funcNr]['memory.enter'];
+                $this->functions[$parts[1]]['time.exit']   = $parts[3];
+                $this->functions[$parts[1]]['time.diff']   = $this->functions[$parts[1]]['time.exit'] - $this->functions[$parts[1]]['time.enter'];
                 break;
             case 'R'; // Function return
-                $this->functions[$funcNr]['return'] = $parts[5];
+                $this->functions[$parts[1]]['return'] = $parts[5];
                 break;
         }
     }
@@ -101,23 +81,23 @@ class XdebugParser
     }
 
     /**
-     * @return false|string
+     * @todo Add background color for each new depth level
+     *
+     * @return string
      */
     public function getTraceHTML()
     {
         ob_start();
 
         echo '<div class="f header">';
-        echo '<div class="func">Function Call</div>';
-        echo '<div class="data">';
-        echo '<span class="file">File:Line</span>';
-        echo '<span class="timediff">ΔTime</span>';
-        echo '<span class="memorydiff">ΔMemory</span>';
-        echo '<span class="time">Time</span>';
-        echo '</div>';
+            echo '<div class="func">Function Call</div>';
+            echo '<div class="data">';
+                echo '<span class="file">File:Line</span>';
+            echo '</div>';
         echo '</div>';
 
         $level = 0;
+        $stripe = '';
         foreach ($this->functions as $func) {
             // depth wrapper
             if ($func['depth'] > $level) {
@@ -131,29 +111,16 @@ class XdebugParser
             }
             $level = $func['depth'];
 
-            $class = 'f';
-            if ($func['internal']) {
-                $class .= ' i';
-            }
-
-            echo '<div class="' . $class . '">';
-
+            echo '<div class="f" style="background-color: ' . $stripe . ';">';
             echo '<div class="func">';
             echo '<span class="name">' . htmlspecialchars($func['name']) . '</span>';
-            echo '(<span class="params short">' . htmlspecialchars(join(",", $func['params'])) . '</span>) ';
-            if ($func['return'] !== '') {
-                echo '→ <span class="return short">' . htmlspecialchars($func['return']) . '</span>';
-            }
             echo '</div>';
-
             echo '<div class="data">';
             echo '<span class="file" title="' . htmlspecialchars($func['file'] . ':' . $func['line']) . '">' . htmlspecialchars(basename($func['file']) . ':' . $func['line']) . '</span>';
-            echo '<span class="timediff">' . sprintf('%f', $func['time.diff']) . '</span>';
-            echo '<span class="memorydiff">' . sprintf('%d', $func['memory.diff']) . '</span>';
-            echo '<span class="time">' . sprintf('%f', $func['time.enter']) . '</span>';
+            echo '</div>';
             echo '</div>';
 
-            echo '</div>';
+            $stripe = ($stripe == 'white') ? 'grey' : 'white';
         }
 
         if ($level > 0) {
